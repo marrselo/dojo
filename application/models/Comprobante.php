6c5158b2dd99cdcb68513777858ef3430b164d71
unknown <Administrador@OTINIANO-ROD.(none)>
2011-12-12 21:20:45 -0500

    comprobante - cat

--- application/models/Comprobante.php
+++ application/models/Comprobante.php
@@ -1,7 +1,7 @@
 <?php
 class Application_Model_Comprobante  extends Zend_Db_Table {
     
-    protected  $_name = "numserie";    
+    protected  $_name = "numeroserie";    
     function listarComprobante(){
         $db = $this->getAdapter();
         $resul = $db->select()
@@ -11,9 +11,35 @@ class Application_Model_Comprobante  extends Zend_Db_Table {
                 'numserie.idtip_doc=.idtip_doc',
                 array())           
             ->query();
-        return $resul;
+        return $resul;        
+}
+    function existeTipo($tipo,$serie){
+        $num = $this->fetchRow(
+                $this->select()->where('idtip_doc = ? ',$tipo)
+                ->where('serie = ? ',$serie));
+        return $num;
+    }
+    function existeComprobante($tipo, $serie, $des){
+        $num = $this->fetchRow(
+                $this->select()->where('idtip_doc = ? ',$tipo)
+                ->where('serie = ? ',$serie)
+                ->where('num = ?', $des)
+                );
+        return $num;        
+    }
+    function maxComprobante($tipo, $serie){
+        $result=$this->fetchRow($this->select()
+                ->from($this, array(new Zend_Db_Expr('max(num) as max')))
+                ->where('idtip_doc = ? ',$tipo)
+                ->where('serie = ? ',$serie)
+                );
+        $max=$result['max'];
+        
+        return $max;
 
         
-}
+        
+    }    
+
 
 }
