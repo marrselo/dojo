d67638cf9363b587198c6e0c659eb261adc4676e
nazart <nazartj@gmail.com>
2012-01-08 11:13:01 -0500

    cambios nazart

--- application/modules/default/controllers/BuscarController.php
+++ application/modules/default/controllers/BuscarController.php
@@ -1,29 +1,29 @@
 <?php
 
-class Default_BuscarController extends ZExtraLib_Controller_Action
-{
+class Default_BuscarController extends ZExtraLib_Controller_Action {
 
     protected $_modelCategorias;
+
     public function init() {
         parent::init();
         $this->_modelCategorias = new Application_Model_Categoria();
     }
-    public function indexAction()
-    {
+
+    public function indexAction() {
         $categorias = $this->_modelCategorias->listaCategorias();
         $this->view->categorias = $categorias;
-        $this->view->menuActive4='active';
+        $this->view->menuActive4 = 'active';
         $filter = new ZExtraLib_SeoUrl();
-        $this->_redirect('/busqueda/'.$filter->filter(trim($this->_getParam('search_query')),'-',0));        
+        $this->_redirect('/busqueda/' . $filter->filter(trim($this->_getParam('search_query')), '-', 0));
     }
-    public function busquedaAction(){
+
+    public function busquedaAction() {
         $modelBusqueda = new Application_Model_Busqueda();
-        $paginator = Zend_Paginator::factory($modelBusqueda->buscar(str_replace('-',"|",$this->_getParam('slug')))); 
+        $paginator = Zend_Paginator::factory($modelBusqueda->buscar(str_replace('-', "|", $this->_getParam('slug'))));
         $paginator->setCurrentPageNumber($this->_getParam('page'));
-        $paginator->setItemCountPerPage(2);
+        $paginator->setItemCountPerPage(6);
         $this->view->articulos = $paginator;
         $this->view->slug = $this->_getParam('slug');
     }
-    
-}
 
+}
