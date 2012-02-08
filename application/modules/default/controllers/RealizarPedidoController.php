c0899bcdcdc017a9500b6351473cebc68006cd86
nazart <nazartj@gmail.com>
2012-02-08 01:40:11 -0500

    cambios nazart

--- application/modules/default/controllers/RealizarPedidoController.php
+++ application/modules/default/controllers/RealizarPedidoController.php
@@ -85,6 +85,11 @@ class Default_RealizarPedidoController extends ZExtraLib_Controller_Action {
             if ($form->isValid($params)) {
                 $params['idcliente'] = $this->registrarCLiente($params);
                 $this->generarComprobante($params);
+<<<<<<< HEAD
+=======
+                $this->enviarCorreo($params['nombre'], $params['correo'],$params['direccion']);
+                
+>>>>>>> parent of 49e2095... cambios nazart
             } else {
                 
             }
@@ -98,11 +103,14 @@ class Default_RealizarPedidoController extends ZExtraLib_Controller_Action {
     
     function formularioCliente() {
 <<<<<<< HEAD
+<<<<<<< HEAD
         
         $date = new Zend_Date();
         
 =======
 >>>>>>> eec5a725ba9f2d47e7462cdb93b7fe1cb4b4ce64
+=======
+>>>>>>> parent of 49e2095... cambios nazart
         $form = new Application_Form_FormCliente();
         $form->getElement('dni')->removeValidator('ZExtraLib_Validate_DniExist');
         $form->getElement('dni')->setRequired();
@@ -117,7 +125,11 @@ class Default_RealizarPedidoController extends ZExtraLib_Controller_Action {
         $form->addElement(new Zend_Form_Element_Radio('tipoPago', array('requerid' => true, 'label' => 'Tipo de Pago', 'multiOptions' => $tiposPago)));
         $form->addElement(new Zend_Form_Element_Text('fechaEntrega',
                         array('label' => 'Fecha Entrega')));
+<<<<<<< HEAD
         
+=======
+
+>>>>>>> parent of 49e2095... cambios nazart
         $form->getElement('fechaEntrega')->setRequired();
         foreach (range(0, 23) as $index):
             $value = strlen($index) == 1 ? '0' . $index : $index;
@@ -168,8 +180,13 @@ class Default_RealizarPedidoController extends ZExtraLib_Controller_Action {
         $data['telefono2'] = $params['telefono2'];
         $data['movil'] = $params['movil'];
         $data['ruc'] = $params['ruc'];
+<<<<<<< HEAD
         if ($cliente = $this->_clienteModel->verificarCLienteWeb($params['correo'],$params['dni'])) {
             $idcliente = $this->_clienteModel->actualizarCliente($cliente['idcliente'],$data);
+=======
+        if ($cliente = $this->_clienteModel->verificarCLienteWeb($params['correo'], $params['dni'])) {
+            $idcliente = $this->_clienteModel->actualizarCliente($cliente['idcliente'], $data);
+>>>>>>> parent of 49e2095... cambios nazart
         } else {
             $idcliente = $this->_clienteModel->crearCliente($data);
         }
@@ -208,8 +225,146 @@ class Default_RealizarPedidoController extends ZExtraLib_Controller_Action {
         $data2['total'] = $total+$this->_config['precioenvio'];
         $this->_documentoModel->actualizarDocumento($data2, $idDocumento);
     }
+<<<<<<< HEAD
     function enviarCorreo($params){
         
+=======
+
+    function enviarCorreo($nombreUsuario, $email,$direccion) {
+        $correo = Zend_Registry::get('mail');
+        $correo = new Zend_Mail('utf-8');
+        $apodo = 'nazart';
+        $body = '<div>
+    <table style="font-family:Verdana,sans-serif;font-size:11px;color:#374953;width:550px">
+        <tbody>
+            <tr>
+                <td align="left">
+                    <a target="_blank" href="' . $this->view->baseUrl() . '" title="Delivery Premiun">
+                        <img alt="Delivery Premium" 
+                             src="' . $this->view->baseUrl() . '/f/img/logo.png" 
+                             style="border:none">
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">Hola <strong style="color:#9c0038">
+                ' . $nombreUsuario . '
+                </strong>, gracias por comprar en <strong>Delivery Premiun</strong>.</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td align="left" 
+                style="background-color:#9c0038;color:#fff;font-size:12px;font-weight:bold;padding:0.5em 1em">
+                Detalles del Pedido
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td align="left">Pedido: 
+                <strong><span style="color:#9c0038">#000006</span> 
+                realizado el 2012-01-05 23:58:58
+                </strong> <br>Forma de Pago: <strong>Pago contra reembolso</strong></td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td align="left">
+                    <table style="width:100%;font-size:11px;color:#374953">
+                        <tbody>
+                            <tr style="background-color:#b9babe;text-align:center">
+                                <th>Producto</th>
+                                <th style="width:15%">Precio Unidad</th>
+                                <th style="width:15%">Cantidad</th>
+                                <th style="width:20%">Precio Total</th>
+                            </tr>';
+        $totalPorductos = 0;
+        foreach ($this->session->listaArticulo as $index) {
+            $body .= '<tr style="background-color:#ebecee">
+                                <td style="padding:0.6em 0.4em"><strong>' . $index['nombre'] . '</strong></td>
+                                    <td style="padding:0.6em 0.4em;text-align:right">
+                                    S/. ' . $index['precioventa'] . '</td>
+                                <td style="padding:0.6em 0.4em;text-align:center">
+                                    ' . $index['cantidadArticulo'] . '
+                                </td>
+                                <td style="padding:0.6em 0.4em;text-align:right">
+                                    S/. ' . $index['cantidadArticulo'] * $index['precioventa'] . '
+                                        </td>
+                            </tr>';
+            $totalPorductos = + $index['cantidadArticulo'] * $index['precioventa'];
+        }
+        $body.='<tr style="text-align:right">
+                                <td colspan="3" style="background-color:#b9babe;padding:0.6em 0.4em">Precio Productos</td>
+                                <td style="background-color:#b9babe;padding:0.6em 0.4em">' . $totalPorductos . '</td>
+                            </tr>
+                            <tr style="text-align:right">
+                                <td colspan="3" style="background-color:#dde2e6;padding:0.6em 0.4em">Gastos de Envíos</td>
+                                <td style="background-color:#dde2e6;padding:0.6em 0.4em">' . $this->_config['precioenvio'] . '</td>
+                            </tr>
+                            <tr style="text-align:right;font-weight:bold">
+                                <td colspan="3" style="background-color:#f1aecf;padding:0.6em 0.4em">TOTAL</td>
+                                <td style="background-color:#f1aecf;padding:0.6em 0.4em">S/. ' . ($totalPorductos + $this->_config['precioenvio']) . '</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <table style="width:100%;font-size:11px;color:#374953">
+                        <tbody>
+                            <tr style="background-color:#b9babe;text-transform:uppercase">
+                                <th style="text-align:left;padding:0.3em 1em">Dirección de Entrega</th>
+                            </tr>
+                            <tr>
+                                <td style="padding:0.5em 0 0.5em 0.5em;background-color:#ebecee">
+                                    <span style="color:#9c0038;font-weight:bold">' . $nombreCliente . '</span> 
+                                    <br>' . $direccion . '
+                                    
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td align="center" style="font-size:10px;border-top:1px solid #d9dade">
+                    <a target="_blank" 
+                       href="' . $this->view->baseUrl() . '" 
+                       style="color:#9c0038;font-weight:bold;text-decoration:none">
+                        Delivery Premiun
+                    </a> 
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>';
+        
+        $subject = 'contacto';
+        try {
+            $correo->addTo($email, $apodo)
+                    ->clearSubject()
+                    ->setSubject('detalle de tu compra')
+                    ->setBodyHtml($body);
+            $correo->send();
+            $message = "Su correo fue enviado Satisfactoriamente";
+        } catch (Exception $e) {
+            //echo 'mensaje->'.$e->getTraceAsString();
+            $message = "Problemas al enviar el correo";
+        }
+        $this->_flashMessenger->addMessage($message);
+        echo $body;
+>>>>>>> parent of 49e2095... cambios nazart
     }
 }
 
