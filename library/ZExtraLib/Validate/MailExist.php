f9e5ba4b44e82e1dafc3046d683115ae4f2cd3bc
nazart <nazartj@gmail.com>
2012-02-17 02:35:13 -0500

    cambios nazart

--- library/ZExtraLib/Validate/MailExist.php
+++ library/ZExtraLib/Validate/MailExist.php
@@ -4,12 +4,11 @@ class ZExtraLib_Validate_MailExist extends Zend_Validate_Abstract{
     const MessageEmailValidator = '';
     
     protected $_messageTemplates = array(
-        self::MessageEmailValidator => "El correo '%value%' Se encuentra registrado por otro usuario"
+        self::MessageEmailValidator => "El correo '%value%' Se encuentra registrado por otro usuario, use otro correo porfavor"
     );
     function isValid($value) {
         $session = new Zend_Session_Namespace('dojo');
         $this->_setValue($value);
-        echo $session->idCliente.'asda';
         $modelCliente = new Application_Model_Cliente();
         if(($modelCliente->verificarEmailCliente($value,$session->idCliente))){
             $this->_error(self::MessageEmailValidator);
